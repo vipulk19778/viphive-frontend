50582dc82384b1245f9d622a5a60ade26481f531
Vipul Kumar <vipulk19778@gmail.com>
2026-09-23 00:46:44 +0530

    fix(logo): add cursor and rounded styles to logo images for better interactivity

--- src/components/common/ViphiveLogo.tsx
+++ src/components/common/ViphiveLogo.tsx
@@ -8,7 +8,7 @@ export function ViphiveLogo({ compact = false }: ViphiveLogoProps) {
       <img
         src="/favicon.ico"
         alt="VIPHive"
-        className="h-9 w-9 object-contain"
+        className="h-9 w-9 object-contain cursor-pointer rounded-lg"
       />
     );
   }
@@ -17,7 +17,7 @@ export function ViphiveLogo({ compact = false }: ViphiveLogoProps) {
     <img
       src="/VIPHive_logo_light.png"
       alt="VIPHive - More than a marketplace"
-      className="h-auto w-32 object-contain sm:w-40"
+      className="h-auto w-32 object-contain sm:w-40 bg-primary dark:bg-transparent cursor-pointer rounded-lg"
     />
   );
 }
